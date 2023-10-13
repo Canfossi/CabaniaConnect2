b8a87cd6f170ec4c5fe67441eea2f94c8136a3e6
carloAnfo <c.anfossi@duocuc.cl>
2023-10-12 23:06:30 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'CabaniaConnect2',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;

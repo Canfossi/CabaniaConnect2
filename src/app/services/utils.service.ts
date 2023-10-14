f9741445b7188018f06162f9efb8767b8f3bb29c
carloAnfo <c.anfossi@duocuc.cl>
2023-10-14 16:27:18 -0300

    creacion del  home

--- src/app/services/utils.service.ts
+++ src/app/services/utils.service.ts
@@ -1,6 +1,6 @@
 import { Injectable,inject } from '@angular/core';
 import { Router } from '@angular/router';
-import { LoadingController, ToastController, ToastOptions } from '@ionic/angular';
+import { LoadingController, ModalController, ModalOptions, ToastController, ToastOptions } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +9,8 @@ export class UtilsService {
 
 loadingCtrl=inject(LoadingController);
 toastCtrl = inject(ToastController);
-router = inject(Router)
+modalCtrl=inject(ModalController);
+router = inject(Router);
 
 //===================loading===========================
 loading(){
@@ -42,7 +43,20 @@ return  JSON.parse(localStorage.getItem(key));
 
 }
 
+//====================modal===========================
+async presentModal(opts: ModalOptions) {
+  const modal = await this.modalCtrl.create(opts);
 
+  await modal.present();
+  const{data}=await modal.onWillDismiss();
+  if(data) return data;
+
+}
+
+dismissModal(data?:any){
+return this.modalCtrl.dismiss(data);
+
+}
 
 
 
---
+++ src/app/shared/components/add-update-product/add-update-product.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-update-product',
+  templateUrl: './add-update-product.component.html',
+  styleUrls: ['./add-update-product.component.scss'],
+})
+export class AddUpdateProductComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}

37af5d6e765b0cd51ce0146b247d7e36396ecb86
carloAnfo <c.anfossi@duocuc.cl>
2023-10-13 23:38:05 -0300

    artos cambios

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { CustomInputComponent } from './components/custom-input/custom-input.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
+
+@NgModule({
+  declarations: [HeaderComponent,
+    CustomInputComponent,
+    LogoComponent
+  ],
+  exports:[HeaderComponent,
+    CustomInputComponent,
+    LogoComponent
+  ],
+  imports: [
+    CommonModule,
+    //vamos a utilizar componentes de ionic
+    IonicModule,
+    //utilizaremos formulario de ionic
+    ReactiveFormsModule,
+    FormsModule
+  ]
+})
+export class SharedModule { }

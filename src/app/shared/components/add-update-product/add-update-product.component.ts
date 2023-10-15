d384b3686de5cb4601ca60fd3a73918299326b1a
carloAnfo <c.anfossi@duocuc.cl>
2023-10-15 09:45:47 -0300

    crear la funcion de editar prodcuto

--- src/app/shared/components/add-update-product/add-update-product.component.ts
+++ src/app/shared/components/add-update-product/add-update-product.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit,inject } from '@angular/core';
+import { Component, Input, OnInit,inject } from '@angular/core';
 import { FormControl,  FormGroup,  Validators } from '@angular/forms';
+import { Product } from 'src/app/models/product.model';
 import { User } from 'src/app/models/user.model';
 import { FirebaseService } from 'src/app/services/firebase.service';
 import { UtilsService } from 'src/app/services/utils.service';
@@ -10,14 +11,17 @@ import { UtilsService } from 'src/app/services/utils.service';
   templateUrl: './add-update-product.component.html',
   styleUrls: ['./add-update-product.component.scss'],
 })
+
 export class AddUpdateProductComponent  implements OnInit {
 
+    @Input()product:Product
+
   form = new FormGroup({
-    id:new FormControl(''),
-    image:new FormControl('',[Validators.required]),
-    name:new FormControl('',[Validators.required,Validators.minLength(4)]),
-    price:new FormControl('',[Validators.required,Validators.min(0)]),
-    soldUnits:new FormControl('',[Validators.required,Validators.min(0)]),
+    id: new FormControl(''),
+    image: new FormControl('',[Validators.required]),
+    name: new FormControl('',[Validators.required,Validators.minLength(4)]),
+    price: new FormControl('',[Validators.required,Validators.min(0)]),
+    soldUnits: new FormControl('',[Validators.required,Validators.min(0)]),
 })
 
 firebaseSvc = inject (FirebaseService);
@@ -37,13 +41,20 @@ this.form.controls.image.setValue(DataUrl);
 
 }
 
-async submit(){
+    submit(){
 
-  let path=`users/${this.user.uid}/products`
+      
+    }
+
+//sirve para crear un producto
+async createProduct(){
 
   if (this.form.valid) {
 
+    let path=`users/${this.user.uid}/products`
+
     const loading=await this.utilsSvc.loading();
+
     await loading.present();
     //=============subir la imagen y obtener la url
     let dataUrl=this.form.value.image;
@@ -93,10 +104,64 @@ async submit(){
 }
 
 
+//===============================Editar un producto=========================================
 
 
+async updateProduct(){
+
+  if (this.form.valid) {
+
+    let path=`users/${this.user.uid}/products`
+
+    const loading=await this.utilsSvc.loading();
+
+    await loading.present();
+    //=============subir la imagen y obtener la url
+    let dataUrl=this.form.value.image;
+    let imagePath=`${this.user.uid}/${Date.now()}`;
+    let imageUrl=await this.firebaseSvc.uploadImage(imagePath,dataUrl);
+    this.form.controls.image.setValue(imageUrl);
+
+    delete this.form.value.id
 
 
+    this.firebaseSvc.addDocument(path,this.form.value).then(async res=>{
+
+      this.utilsSvc.dismissModal({success:true});
+
+
+    this.utilsSvc.presentToast({
+
+      message: "producto agregado exitosamente",
+      duration: 1500,
+      color:'success',
+      position:'middle',
+      icon:'checkmark-circle-outline'
+  })
+
+   
+
+    
+
+  }).catch(error=>{
+    console.log(error)
+
+    this.utilsSvc.presentToast({
+
+        message: error.message,
+        duration: 2500,
+        color:'primary',
+        position:'middle',
+        icon:'alert-circle-outline'
+    })
+
+
+    }).finally(()=>{
+      loading.dismiss();
+    })
+  }
+  
+}
 
 
 }
